f0f326219132e017b37ec4230d93f4515550462f
PranayDange <pranaydange04@gmail.com>
2023-03-13 22:28:46 +0530

    might delete later

---
+++ src/main/java/com/example/onemoretime/basic/HcfLcm.java
@@ -0,0 +1,9 @@
+package com.example.onemoretime.basic;
+
+public class HcfLcm {
+    public static void main(String[] args) {
+        System.out.printf("Hello world");
+        System.out.printf("Hello world");
+        System.out.printf("Hello world");
+    }
+}
